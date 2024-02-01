32608d74744ecded3b87db18a587af97cdd080e1
tbonest3ak5 <e72huang@uwaterloo.ca>
2024-02-01 06:50:44 -0500

    Update styles.js

--- src/styles.js
+++ src/styles.js
@@ -9,7 +9,7 @@ const styles = {
       "text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px]",
     heroCommentText:
       "text-[#dfd9ff] font-small lg:text-[25px] sm:text-[20px] xs:text-[16px] text-[10px] lg:leading-[30px]",
-    
+    //comment
   
     sectionHeadText:
       "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
